46d6bfa6046f05c652c9e0624101e32361fa05be
skfh5970841 <leechangwan1012@gmail.com>
2023-07-11 13:15:38 +0900

    HabitTrackerSetting is implemented

--- src/components/Habit/HabitTrackerSetting.js
+++ src/components/Habit/HabitTrackerSetting.js
@@ -14,6 +14,11 @@ const HabitTracker = () => {
   const [selectedHabit, setSelectedHabit] = useState(null);
   const [editing, setEditing] = useState(false);
   const [showForm, setShowForm] = useState(false);
+  const [editingTitle, setEditingTitle] = useState("");
+  const [editingPeriod, setEditingPeriod] = useState("");
+  const [editingDescription, setEditingDescription] = useState("");
+  const [editingStartTime, setEditingStartTime] = useState("00:00");
+  const [editingEndTime, setEditingEndTime] = useState("23:59");
 
   const days = [
     { day: "월", order: 1 },
@@ -105,11 +110,11 @@ const HabitTracker = () => {
     updatedHabits[habitIndex] = {
       ...selectedHabit,
       date: selectedDate.toISOString(),
-      startTime,
-      endTime,
+      startTime: editingStartTime,
+      endTime: editingEndTime,
       days: selectedDays,
-      title,
-      description,
+      title: editingTitle,
+      description: editingDescription,
     };
 
     setHabits(updatedHabits);
@@ -198,7 +203,83 @@ const HabitTracker = () => {
       ))}
       {selectedHabit && (
         <div className="HabitDetails">
-          <h2>{selectedHabit.title}</h2>
+          {!editing ? (
+            <>
+              <h2>{selectedHabit.title}</h2>
+              <h4>
+                시작 날짜: {new Date(selectedHabit.date).toLocaleDateString()}
+              </h4>
+              <h4>주기: {selectedHabit.days.map((d) => d.day).join(", ")}</h4>
+              <h4>상세 정보: {selectedHabit.description}</h4>
+              <h4>
+                시작 시간: {selectedHabit.startTime} / 종료 시간:{" "}
+                {selectedHabit.endTime}
+              </h4>
+              <button onClick={() => setEditing(true)}>습관 편집</button>
+              <button onClick={() => handleDelete(selectedHabit.id)}>
+                습관 삭제
+              </button>
+            </>
+          ) : (
+            <>
+              <h2>
+                <label>습관 제목 : </label>
+                <input
+                  placeholder="습관 제목 임마"
+                  value={editingTitle}
+                  onChange={(e) => setEditingTitle(e.target.value)}
+                ></input>
+              </h2>
+              <h4>
+                <label>반복 주기: </label>
+                {days.map((day) => (
+                  <span
+                    key={day.day}
+                    onClick={() => handleClick(day)}
+                    style={{
+                      fontWeight: selectedDays.some((d) => d.day === day.day)
+                        ? "bold"
+                        : "normal",
+                      margin: "0 5px",
+                      cursor: "pointer",
+                    }}
+                  >
+                    {day.day}
+                  </span>
+                ))}
+                <div>
+                  선택된 요일:{" "}
+                  {selectedDays.length > 0
+                    ? selectedDays.map((d) => d.day).join(", ")
+                    : "없음"}
+                </div>
+              </h4>
+              <h4>
+                상세 정보:{" "}
+                <input
+                  placeholder="상세 정보 임마"
+                  value={editingDescription}
+                  onChange={(e) => setEditingDescription(e.target.value)}
+                ></input>
+              </h4>
+              <div className="TimeSelect">
+                <h3>시작 시간 / 종료 시간</h3>
+                <TimePicker
+                  onChange={setEditingStartTime}
+                  value={editingStartTime}
+                />
+                {" 부터 "}
+                <TimePicker
+                  onChange={setEditingEndTime}
+                  value={editingEndTime}
+                />
+              </div>
+              <button onClick={() => handleEdit(selectedHabit.id)}>
+                편집 완료
+              </button>
+            </>
+          )}
+          {/* <h2>{selectedHabit.title}</h2>
           <h4>
             시작 날짜: {new Date(selectedHabit.date).toLocaleDateString()}
           </h4>
@@ -219,7 +300,7 @@ const HabitTracker = () => {
             <button onClick={() => handleEdit(selectedHabit.id)}>
               편집 완료
             </button>
-          )}
+          )} */}
         </div>
       )}
     </>
